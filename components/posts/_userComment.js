f019cf24ad9506bdda421e2be946ebfccfe5abfb
Salman Thasleem <salmanthasleem@gmail.com>
2022-04-28 16:50:49 +0530

    comment prediction done

--- components/posts/_userComment.js
+++ components/posts/_userComment.js
@@ -5,33 +5,55 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function _userComment({
   _id,
   comment,
   createdAt,
   post,
+  severity,
   ...props
 }) {
+  const filteredSeverity = Object.keys(severity)
+    .reduce((arr, item, i, array) => {
+      if (severity[item] > 70) {
+        const filteredKey = item
+          .replace(/[^a-zA-Z ]/g, " ")
+          .split(" ")
+          .map((item) => item[0].toUpperCase() + item.substring(1))
+          .join(" ")
+          .trim();
+        const val = `${filteredKey}: ${severity[item]}%`;
+        arr.push(val);
+        return arr;
+      }
+      return arr;
+    }, [])
+    .join("\n");
   const date = new Date(createdAt) || new Date();
   return (
-    <Box
-      sx={{
-        maxWidth: 1,
-        display: "flex",
-        flexDirection: "column",
-        margin: 2,
-      }}
+    <Tooltip
+      title={<div style={{ whiteSpace: "pre-line" }}>{filteredSeverity}</div>}
     >
-      <Typography sx={{ fontSize: 10 }} color="text.secondary" gutterBottom>
-        User Name(N/a)
-      </Typography>
-      <Typography sx={{ fontSize: 12 }} variant="body1" component="div">
-        {comment}
-      </Typography>
-      <Typography sx={{ fontSize: 8 }} color="text.secondary">
-        {date.toLocaleString()}
-      </Typography>
-    </Box>
+      <Box
+        sx={{
+          maxWidth: 1,
+          display: "flex",
+          flexDirection: "column",
+          margin: 2,
+        }}
+      >
+        <Typography sx={{ fontSize: 10 }} color="text.secondary" gutterBottom>
+          User Name(N/a)
+        </Typography>
+        <Typography sx={{ fontSize: 12 }} variant="body1" component="div">
+          {comment}
+        </Typography>
+        <Typography sx={{ fontSize: 8 }} color="text.secondary">
+          {date.toLocaleString()}
+        </Typography>
+      </Box>
+    </Tooltip>
   );
 }
